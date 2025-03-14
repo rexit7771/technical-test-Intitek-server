90febb923364d13ea46661563a80bde7fbeb52e8
Muhammad Bagus Pramaskoro <pramaskoro@gmail.com>
2025-03-15 06:16:34 +0700

    Change DB Config

--- database/config.go
+++ database/config.go
@@ -15,26 +15,27 @@ var (
 )
 
 func ConnectDB() {
-	// if err := godotenv.Load(); err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
+	// Check both naming conventions for flexibility
+	dbUser := getEnvWithFallback("MYSQLUSER", "DB_USER")
+	dbPass := getEnvWithFallback("MYSQLPASSWORD", "DB_PASSWORD")
+	dbHost := getEnvWithFallback("MYSQLHOST", "DB_HOST")
+	dbPort := getEnvWithFallback("MYSQLPORT", "DB_PORT")
+	dbName := getEnvWithFallback("MYSQLDATABASE", "DB_NAME")
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		os.Getenv("MYSQLUSER"),
-		os.Getenv("MYSQLPASSWORD"),
-		os.Getenv("MYSQLHOST"),
-		os.Getenv("MYSQLPORT"),
-		os.Getenv("MYSQLDATABASE"),
-		// os.Getenv("DB_USER"),
-		// os.Getenv("DB_PASSWORD"),
-		// os.Getenv("DB_HOST"),
-		// os.Getenv("DB_PORT"),
-		// os.Getenv("DB_NAME"),
-	)
+		dbUser, dbPass, dbHost, dbPort, dbName)
 
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Failed to connect to the database", err)
+		log.Fatal("Failed to connect to the database: ", err)
 	}
 	fmt.Println("Database Connected")
+}
+
+// Helper function to check multiple environment variable names
+func getEnvWithFallback(key, fallbackKey string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return os.Getenv(fallbackKey)
 }
